a295ad3fac74afdd105e20227060dbed51a86dbc
Patrick Montenegro <pmontenegro21@gmail.com>
2022-12-14 15:10:07 +0100

    Commencement

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+// requêtes des data 
+ 
+
+// requêtes des templates
+require_once __DIR__ . '/inc/templates/header.tpl.php';
+require_once __DIR__ . '/inc/templates/home.tpl.php';
+require_once __DIR__ . '/inc/templates/footer.tpl.php';
